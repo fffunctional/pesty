466b3b6916e42b736d3e6ef59df8fb5f8a624198
Matthew Marcus <matthew.marcus@hey.com>
2024-06-14 17:17:09 -0700

    Episode 9

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,13 +1,21 @@
 <?php
 
+use App\Models\Course;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 use function Pest\Laravel\get;
 
 uses(RefreshDatabase::class);
 
-it('returns a successful response', function () {
+it('returns a successful response for the home page', function () {
     // Act & Assert
     get(route('home'))
         ->assertOk();
 });
+
+it('returns a successful response for course details page', function () {
+   $course = Course::factory()->create();
+   get(route('course-details', $course))
+        ->assertOk();
+});
+
